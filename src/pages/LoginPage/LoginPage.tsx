828b50546bc8ee2478d8efc7bdfbe14bdeacb67a
Daniel-1-1 <bes.dan2017@ya.ru>
2024-03-02 12:34:11 +0300

    add widgets routing

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,24 @@
-import React from "react";
-import LoginStart from "../../widgets/LoginStart/LoginStart.tsx";
+import React, { useState } from 'react';
+import {LoginStart} from "../../widgets/LoginStart/LoginStart.tsx";
+import {LoginInputEmail} from "../../widgets/LoginInputEmail/LoginInputEmail.tsx";
+import {LoginInputCode} from "../../widgets/LoginInputCode/LoginInputCode.tsx";
+
 const LoginPage: React.FC = () => {
+    // Состояние для отслеживания текущего шага входа
+    const [loginStep, setLoginStep] = useState<'start' | 'inputEmail' | 'inputCode'>('start');
+
+    // Функции для изменения текущего шага входа
+    const goToInputEmail = () => setLoginStep('inputEmail');
+    const goToInputCode = () => setLoginStep('inputCode');
+
     return (
-        <LoginStart />
+        <div>
+            {loginStep === 'start' && <LoginStart onNextStep={goToInputEmail} />}
+            {loginStep === 'inputEmail' && <LoginInputEmail onNextStep={goToInputCode} />}
+            {loginStep === 'inputCode' && <LoginInputCode />}
+        </div>
     );
 };
 
-export {LoginPage};
+export default LoginPage;
+
