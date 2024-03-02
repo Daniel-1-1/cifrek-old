133a03c3cbeeb17d165131bd52a26f289d16dc1f
Daniel-1-1 <bes.dan2017@ya.ru>
2024-03-02 12:34:57 +0300

    add screen with code

---
+++ src/widgets/LoginInputCode/LoginInputCode.tsx
@@ -0,0 +1,16 @@
+// LoginInputCode.tsx
+import React from 'react';
+
+const LoginInputCode: React.FC = () => {
+    return (
+        <div>
+            {/* Контент виджета */}
+            <p>Введите код подтверждения:</p>
+            <input type="text" />
+
+            {/* Дополнительные элементы, кнопки и т.д. для этого виджета */}
+        </div>
+    );
+};
+
+export { LoginInputCode };
