c72d4d72bc178811cd47f881fd976956b4b2da8f
Daniel-1-1 <bes.dan2017@ya.ru>
2024-03-02 12:32:51 +0300

    styles for LoginEmail

---
+++ src/entities/Login/ui/LoginInputEmail/LoginInputEmail.styled.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+
+export const LoginEmailContainer = styled.div`
+    background: #14904C;
+    font-family: "Montserrat Light";
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    
+`
+
+export const LoginEmailTitle = styled.h1`
+    color: white;
+    font-size: 20px;
+    margin-bottom: 30px;
+    margin-top: 260px;
+    text-align: center;
+`
+
+export const LoginEmailError = styled.p`
+    color: #fff300;
+    margin-bottom: 6px;
+`
